61472366e983f38e44855d85b5deb059f08b5110
Unknown <kurupical@gmail.com>
2020-10-05 18:35:31 +0900

    FeatureProcessor_validate

--- backtestforstock/features/core.py
+++ backtestforstock/features/core.py
@@ -17,6 +17,15 @@ class FeatureProcessor:
         """
         raise NotImplementedError
 
+    def _validate(self,
+                  df_original: pd.DataFrame,
+                  df_transform: pd.DataFrame):
+
+        expect_same_cols = ["date", "code", "open", "close", "high", "low", "volume"]
+
+        pd.testing.assert_frame_equal(df_original[expect_same_cols],
+                                      df_transform[expect_same_cols])
+
     def transform(self,
                   df: pd.DataFrame):
         """
@@ -25,4 +34,8 @@ class FeatureProcessor:
         :return: df: 加工された特徴量データ
         """
 
-        return self._transform_core(copy.copy(df))
+        df_ret = self._transform_core(copy.copy(df))
+        self._validate(df_original=df,
+                       df_transform=df_ret)
+
+        return df_ret
